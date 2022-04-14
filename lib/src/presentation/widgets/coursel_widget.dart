8f968554151caf427386a6a2b30866236c75229e
ropi1995 <ropiudin@dkatalis.com>
2022-04-14 15:43:15 +0700

    [CHORES] : dangling comma

--- lib/src/presentation/widgets/coursel_widget.dart
+++ lib/src/presentation/widgets/coursel_widget.dart
@@ -10,57 +10,58 @@ class CourselWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.only(top: 8.0),
-        height: 200,
-        child: CarouselSlider.builder(
-          options: CarouselOptions(
-            height: 400,
-            aspectRatio: 16 / 9,
-            viewportFraction: 0.8,
-            initialPage: 0,
-            enableInfiniteScroll: true,
-            reverse: false,
-            autoPlay: true,
-            autoPlayInterval: const Duration(seconds: 3),
-            autoPlayAnimationDuration: const Duration(milliseconds: 800),
-            autoPlayCurve: Curves.fastOutSlowIn,
-            enlargeCenterPage: true,
-            scrollDirection: Axis.horizontal,
-          ),
-          itemCount: article.length,
-          itemBuilder:
-              (BuildContext context, int itemIndex, int pageViewIndex) => Stack(
-            children: [
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetailScreen(
-                        article: article[itemIndex],
-                      ),
+      margin: const EdgeInsets.only(top: 8.0),
+      height: 200,
+      child: CarouselSlider.builder(
+        options: CarouselOptions(
+          height: 400,
+          aspectRatio: 16 / 9,
+          viewportFraction: 0.8,
+          initialPage: 0,
+          enableInfiniteScroll: true,
+          reverse: false,
+          autoPlay: true,
+          autoPlayInterval: const Duration(seconds: 3),
+          autoPlayAnimationDuration: const Duration(milliseconds: 800),
+          autoPlayCurve: Curves.fastOutSlowIn,
+          enlargeCenterPage: true,
+          scrollDirection: Axis.horizontal,
+        ),
+        itemCount: article.length,
+        itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) =>
+            Stack(
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(
+                      article: article[itemIndex],
                     ),
-                  );
-                },
-                child: Image.network(
-                  article[itemIndex].urlToImage,
+                  ),
+                );
+              },
+              child: Image.network(
+                article[itemIndex].urlToImage,
+                width: MediaQuery.of(context).size.width - 92,
+                height: 400,
+                fit: BoxFit.fill,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                  height: 40,
                   width: MediaQuery.of(context).size.width - 92,
-                  height: 400,
-                  fit: BoxFit.fill,
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomLeft,
-                child: Container(
-                    height: 40,
-                    width: MediaQuery.of(context).size.width - 92,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                    ),
-                    child: Text(article[itemIndex].title)),
-              ),
-            ],
-          ),
-        ));
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Text(article[itemIndex].title)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
